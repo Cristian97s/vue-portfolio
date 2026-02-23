779217b966363ef090252b727bccde5b0f258522
Cristian97s <cristianaleman1997@gmail.com>
2026-02-22 20:54:27 -0600

    feat: Agregando apartados en app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup>
 import Navbar from './components/layout/Navbar.vue';
 import Hero from './components/sections/Hero.vue';
+import Skills from './components/sections/Skills.vue';
+import Experience from './components/sections/Experience.vue';
+import Projects from './components/sections/Projects.vue';
+import Contact from './components/sections/Contact.vue';
 </script>
 
 <template>
   <Navbar />
   <main>
     <Hero />
+    <Skills />
+    <Experience />
+    <Projects />
+    <Contact />
   </main>
 </template>
 
